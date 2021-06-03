762855b9104eb6b41ac603b5b51864d0c774e7c9
Brandon Roff <45691205+Roffo66@users.noreply.github.com>
2021-06-03 11:04:03 +0100

    Update Binary_Blast.sh

--- Binary_Blast.sh
+++ Binary_Blast.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
 
-PASSWORD=upvgtnar666@
-num=235
-host=192.168.195.${num}
-	[ ! -d /NAS/IT/RPI/Pi-Sever/PiBlast ] && mkdir -p /NAS/IT/RPI/Pi-Sever/PiBlast/
-	localpath=/NAS/IT/RPI/Pi-Sever/PiBlast/
+PASSWORD=ENTER YOUR PASS HERE
+num=235 #host end ip number
+host=192.168.195.${num} #subnet ip
+	[ ! -d ~/PiBlast ] 
+	localpath=~/PiBlast/
 	remotepath="${host}:/usr/local/bin"
 	#echo $remotepath
-	sudo rsync -avz --rsh="sshpass -p ${PASSWORD} ssh -l root -oStrictHostKeyChecking=accept-new" "$localpath" "$remotepath"
+	sudo rsync -avz --rsh="sshpass -p ${PASSWORD} ssh -l USER -oStrictHostKeyChecking=accept-new" "$localpath" "$remotepath"
 
-PASSWORD=upvgtnar666@
-num=53
-host=192.168.195.${num}
-	[ ! -d /NAS/IT/RPI/Pi-Sever/PiBlast ] && mkdir -p /NAS/IT/RPI/Pi-Sever/PiBlast/
-	localpath=/NAS/IT/RPI/Pi-Sever/PiBlast/
-	remotepath="${host}:/usr/local/bin"
-	#echo $remotepath
-	sudo rsync -avz --rsh="sshpass -p ${PASSWORD} ssh -l root -oStrictHostKeyChecking=accept-new" "$localpath" "$remotepath"
 
-PASSWORD=upvgtnar666@
-num=7
-host=192.168.195.${num}
-	[ ! -d /NAS/IT/RPI/Pi-Sever/PiBlast ] && mkdir -p /NAS/IT/RPI/Pi-Sever/PiBlast/
-	localpath=/NAS/IT/RPI/Pi-Sever/PiBlast/
-	remotepath="${host}:/usr/local/bin"
-	#echo $remotepath
-	sudo rsync -avz --rsh="sshpass -p ${PASSWORD} ssh -l root -oStrictHostKeyChecking=accept-new" "$localpath" "$remotepath"
